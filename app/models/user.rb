846cb718f5c7757d461907e13c5048228a1f4d5d
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-09-02 17:27:05 +0200

    addmailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :sales
   has_many :items, through: :sales, foreign_key: "user_id"
-  after_create :welcome_send
 
-def welcome_send
-  UserMailer.welcome_email(self).deliver_now
-end
+
+
 end

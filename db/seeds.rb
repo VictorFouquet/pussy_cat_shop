9e8b329daae09c992d481f71e099e151f20f7653
Raphael Chillio--Costantin <raphael.chillio@gmail.com>
2019-09-02 15:34:57 +0200

    routes index_view and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Sale.destroy_all
+Item.destroy_all
+User.destroy_all
+
+10.times do
+
+	password = 343434;
+
+	user = User.create!(user_name: Faker::Name.first_name, email: Faker::Internet.email, password: password, password_confirmation: password)
+  
+	item = Item.create!(title: Faker::Music::RockBand.name, description: Faker::Lorem.paragraph, price: Faker::Number.between(from: 1, to: 15), image_url: Rails.root.join("app/assets/images/kitten-index.jpg").open)
+  
+	sale = Sale.create!(user: User.all.sample, item: Item.all.sample)
+
+end

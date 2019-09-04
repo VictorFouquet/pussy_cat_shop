86077a01c1005a25544e6a706732785374d28444
MatthieuG <matthieu.frederic.gallea@gmail.com>
2019-09-04 17:28:52 +0200

    page HTML emailer admin+user

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'dotenv-rails'
 gem 'devise'
 gem 'stripe'
 
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use postgresql as the database for Active Record
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,9 +1,9 @@
 class AdminMailer < ApplicationMailer
   default from: 'puspicatshop@gmail.com'
 
-  def admin_mailer
+  def admin_mailer(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-
+    @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login'

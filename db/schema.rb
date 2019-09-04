f028f97071d52bb1c82a215a208bbef982531c28
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-09-04 14:53:05 +0200

    add admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_073859) do
+ActiveRecord::Schema.define(version: 2019_09_04_123845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,11 +57,15 @@ ActiveRecord::Schema.define(version: 2019_09_04_073859) do
     t.text "description"
     t.decimal "price"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sales", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_sales_on_item_id"
     t.index ["user_id"], name: "index_sales_on_user_id"
   end
@@ -75,6 +79,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_073859) do
     t.string "user_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin?"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

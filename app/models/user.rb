a5efe1936cb42c2995db16325d979c3d491f2017
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-09-04 16:54:46 +0200

    Add Mailer For Welcome User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,10 @@ class User < ApplicationRecord
   has_many :sales
   has_many :items, through: :sales, foreign_key: "user_id"
 	has_one :cart
+  after_create :welcome_user_email
+
+  def welcome_user_email
+    WelcomeUserMailer.welcome_user_email(self.user).deliver_now
+  end
 
 end
